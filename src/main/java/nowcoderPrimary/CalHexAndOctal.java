2cefda88d1e27ce649ea28a77f227aa1f9b3d13b
fangxiao <shawnfang07@gmail.com>
2020-05-30 20:42:56 +0800

    update

--- src/main/java/nowcoderPrimary/CalHexAndOctal.java
+++ src/main/java/nowcoderPrimary/CalHexAndOctal.java
@@ -1,7 +1,5 @@
 package nowcoderPrimary;
 
-import java.util.Scanner;
-
 /*
 题目描述
 输入一个十六进制数a，和一个八进制数b，输出a+b的十进制结果（范围-231~231-1）。
@@ -17,12 +15,15 @@ import java.util.Scanner;
 备注:
 十六进制Hexadecimal一般以0x开头，例如0xFF。八进制Octal，一般以0开头，例如07。
  */
+
+import java.util.Scanner;
+
 public class CalHexAndOctal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] num = scanner.next().split(" ");
-        int hex = Integer.valueOf(num[0].substring(2),16);
-        int oct = Integer.valueOf(num[1].substring(1),8);
+        String num = scanner.nextLine();
+        int hex = Integer.valueOf(num.split(" ")[0].substring(2),16);
+        int oct = Integer.valueOf(num.split(" ")[1].substring(1),8);
         int total = hex+oct;
         System.out.print(total);
 
