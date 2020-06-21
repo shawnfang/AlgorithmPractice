d9535cde5134de961a69a37387ac6e5bf604b0b8
fangxiao <shawnfang07@gmail.com>
2020-06-21 14:25:42 +0800

    update

---
+++ src/main/java/nowcoderPrimary/BC132.java
@@ -0,0 +1,31 @@
+package nowcoderPrimary;
+
+/*
+题目描述
+KiKi这学期努力学习程序设计基础，要期末考试了，BoBo老师告诉他，总成绩包括四个部分，如下：
+总成绩 = 实验成绩 * 20% + 课堂表现成绩 * 10% + 过程考核成绩 * 20% + 期末上机考试成绩 * 50%，现在输入KiKi的各项成绩，请计算KiKi的总成绩。
+输入描述:
+一行，包括四个整数（百分制），用空格分隔，分别表示实验成绩，课堂表现成绩，过程考核成绩，期末上机考试成绩。
+输出描述:
+一行，总成绩，保留小数点一位。
+示例1
+输入
+100 100 90 80
+输出
+88.0
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BC132 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] strings = bufferedReader.readLine().split(" ");
+        double total =(Double)(Integer.parseInt(strings[0])*0.2+Integer.parseInt(strings[1])*0.1+
+                Integer.parseInt(strings[2])*0.2+Integer.parseInt(strings[3])*0.5);
+        System.out.printf("%s",total);
+
+    }
+}
---
+++ src/main/java/nowcoderPrimary/BC133.java
@@ -0,0 +1,30 @@
+package nowcoderPrimary;
+
+/*
+题目描述
+2020年来到了，KiKi要对他的n位好朋友说n遍”Happy new year!Good luck!”的祝福语。
+输入描述:
+输入只有一行，其中含有一个正整数 n(1≤n≤20)，代表KiKi说祝福话的遍数。
+输出描述:
+输出 n行"Happy new year!Good luck!"。
+示例1
+输入
+2
+输出
+Happy new year!Good luck!
+Happy new year!Good luck!
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BC133 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 1; i <= num ; i++) {
+            System.out.println("Happy new year!Good luck!");
+        }
+    }
+}
