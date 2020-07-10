2e663014901c96229c638b38d557f7ce8109ff4d
shawnfang <shawnfang07@gmail.com>
2020-07-10 18:02:18 +0800

    update

---
+++ src/main/java/nowcodeDataStructures/Board.java
@@ -0,0 +1,18 @@
+package nowcodeDataStructures;
+/*
+题目描述
+对于一个给定的井字棋棋盘，请设计一个高效算法判断当前玩家是否获胜。
+给定一个二维数组board，代表当前棋盘，其中元素为1的代表是当前玩家的棋子，为0表示没有棋子，为-1代表是对方玩家的棋子。
+测试样例：
+[[1,0,1],[1,-1,-1],[1,-1,0]]
+返回：true
+ */
+import java.util.*;
+public class Board {
+    public boolean checkWon(int[][] board) {
+        // write code here
+        boolean result = true;
+        return result;
+    }
+}
+
