6ea61c88f140f007298f7206bea4b4e2f1672ece
fangxiao <shawnfang07@gmail.com>
2020-07-11 15:52:42 +0800

    update

--- src/main/java/nowcodeDataStructures/Board.java
+++ src/main/java/nowcodeDataStructures/Board.java
@@ -11,8 +11,61 @@ import java.util.*;
 public class Board {
     public boolean checkWon(int[][] board) {
         // write code here
-        boolean result = true;
+        boolean result = false;
+        int count = 0;
+        int count2 = 0;
+        int count3 = 0;
+        int count4 = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == 1) {
+                    count++;
+                }
+                if (board[j][i] == 1) {
+                    count2++;
+                }
+                if (board[j][j] == 1) {
+                    count3++;
+                }
+            }
+            break;
+        }
+        System.out.println(count);
+        System.out.println(count2);
+        System.out.println(count3);
+        if (count == 3 || count2 == 3 || count3 ==3 ) {
+            result=true;
+            return result;
+        }
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i <= 2; i++) {
+            for (int j = i; j <= i; j--) {
+                for (int k = i; k <= j ; k++) {
+                    arrayList.add(board[k][j]);
+                    break;
+                }
+                break;
+            }
+
+            if (arrayList.get(0) == 1) {
+                count4++;
+            }
+            arrayList.clear();
+        }
+        System.out.println(count4);
+        if (count4 == 3) {
+            result = true;
+            return  result;
+        }
+
         return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] a = {{1,-1,1},{-1,0,0},{0,0,0}};
+        Board board = new Board();
+        System.out.println(board.checkWon(a));
+
     }
 }
 
