355dcd4c52dac2a4684733be1abbb34f89a39a2f
fangxiao <shawnfang07@gmail.com>
2020-07-11 17:12:33 +0800

    update

---
+++ src/main/java/nowcodeDataStructures/AntiOrder.java
@@ -0,0 +1,31 @@
+package nowcodeDataStructures;
+/*
+题目描述
+有一组数，对于其中任意两个数组，若前面一个大于后面一个数字，
+则这两个数字组成一个逆序对。请设计一个高效的算法，计算给定数组中的逆序对个数。
+给定一个int数组A和它的大小n，请返回A中的逆序对个数。保证n小于等于5000。
+测试样例：
+[1,2,3,4,5,6,7,0],8
+返回：7
+ */
+import java.util.*;
+public class AntiOrder {
+    public int count(int[] A, int n) {
+        // write code here
+        int countNum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                if (A[i] > A[j]) {
+                    countNum++;
+                }
+            }
+        }
+        return countNum;
+    }
+
+    public static void main(String[] args) {
+        AntiOrder antiOrder = new AntiOrder();
+        int[] a = {1,2,3,4,5,6,7,0};
+        System.out.println(antiOrder.count(a,8));
+    }
+}
---
+++ src/main/java/nowcodeDataStructures/ArrSort.java
@@ -0,0 +1,30 @@
+package nowcodeDataStructures;
+/*
+题目描述
+给定一个数组arr，其中只可能含有0、1、2三个值，请实现arr的排序
+[要求]
+时间复杂度为O(n)O(n)，空间复杂度为O(1)O(1)
+
+输入描述:
+第一行一个整数N。表示数组长度
+接下来一行N个整数表示数组内的数
+输出描述:
+输出N个整数，表示排序后的结果
+示例1
+输入
+复制
+5
+2 0 1 2 0
+输出
+复制
+0 0 1 2 2
+备注:
+1 ⩽N⩽10^5
+
+保证arr_i=0/1/2
+ */
+public class ArrSort {
+    public static void main(String[] args) {
+        
+    }
+}
