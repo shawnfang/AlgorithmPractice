c52ae04462e123119e186c4736a3d3bef84e19c9
fangxiao <shawnfang07@gmail.com>
2020-07-13 21:42:45 +0800

    update

--- src/main/java/nowcodeDataStructures/RussiaGame.java
+++ src/main/java/nowcodeDataStructures/RussiaGame.java
@@ -3,6 +3,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/*
+题目描述
+小易有一个古老的游戏机，上面有着经典的游戏俄罗斯方块。因为它比较古老，所以规则和一般的俄罗斯方块不同。
+荧幕上一共有 n 列，每次都会有一个 1 x 1 的方块随机落下，在同一列中，后落下的方块会叠在先前的方块之上，
+当一整行方块都被占满时，这一行会被消去，并得到1分。
+有一天，小易又开了一局游戏，当玩到第 m 个方块落下时他觉得太无聊就关掉了，小易希望你告诉他这局游戏他获得的分数。
+输入描述:
+第一行两个数 n, m
+第二行 m 个数，c1, c2, ... , cm ， ci 表示第 i 个方块落在第几列
+其中 1 <= n, m <= 1000, 1 <= ci <= n
+输出描述:
+小易这局游戏获得的分数
+示例1
+输入
+复制
+3 9
+1 1 2 2 2 3 1 2 3
+输出
+复制
+2
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class RussiaGame {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -26,4 +52,8 @@ public class RussiaGame {
         }
         System.out.println(arr[0]);
     }
+<<<<<<< Updated upstream
 }
+=======
+}
+>>>>>>> Stashed changes
