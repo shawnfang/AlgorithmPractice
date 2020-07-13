1998c4bf2799453debbaed79d88af201faad6a39
shawnfang <shawnfang07@gmail.com>
2020-07-13 09:16:01 +0800

    update

--- src/main/java/nowcodeDataStructures/RussiaGame.java
+++ src/main/java/nowcodeDataStructures/RussiaGame.java
@@ -1,4 +1,29 @@
 package nowcodeDataStructures;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class RussiaGame {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] strings = bufferedReader.readLine().split(" ");
+        int n = Integer.parseInt(strings[0]);
+        int m = Integer.parseInt(strings[1]);
+        int[] arr = new int[n];
+        int temp;
+        String[] strings1 = bufferedReader.readLine().split(" ");
+        for (int i = 0; i < m; i++) {
+            arr[Integer.parseInt(strings1[i])-1]++;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        System.out.println(arr[0]);
+    }
 }
